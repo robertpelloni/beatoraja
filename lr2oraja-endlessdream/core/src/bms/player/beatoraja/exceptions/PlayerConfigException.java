aec8f35c024d362df0e09bd6027f458af3308e6f
Robert Pelloni <pelloni.robert@gmail.com>
2026-06-05 11:41:12 -0400

    fix: update stale lr2oraja-endlessdream pointer

--- lr2oraja-endlessdream/core/src/bms/player/beatoraja/exceptions/PlayerConfigException.java
+++
@@ -1,7 +0,0 @@
-package bms.player.beatoraja.exceptions;
-
-public class PlayerConfigException extends RuntimeException {
-    public PlayerConfigException(String message) {
-        super(message);
-    }
-}
